d0390e2408208f976b0dd1e5e3ca5221480f7069
LandenParke <eddisonparke@gmail.com>
2025-01-25 11:56:19 -0500

    Fixed Firebase SDK in package.json files

--- firebaseDB.js
+++ firebaseDB.js
@@ -1,7 +1,8 @@
 // Firebase Modules
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, push, set } from "firebase/database";
-import APIKeys from "./APIKeys.js"; // Private API Keys
+import APIKeys from "./APIKeys.js"; // API Keys
+
 
 // Your web app's Firebase configuration
 
@@ -12,20 +13,27 @@ const firebaseConfig = {
   projectId: APIKeys.projectId,
   storageBucket: APIKeys.storageBucket,
   messagingSenderId: APIKeys.messagingSenderId,
-  appId: APIKeys.appID
+  appId: APIKeys.appId,
 
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+console.log('Firebase App Initialized:', app.name);
+
 
 export const firebasePushData = (longitude, latitude, imageLink) => {
-  const db = getDatabase(app);
-  const ref = push(ref(db, '/images'));
-  set(ref, {
-    longitude: longitude,
-    latitude: latitude,
-    imageLink: imageLink,
-    time: new Date().getTime()
-  });
+  try {
+    const db = getDatabase(app);
+    const ref = push(ref(db, '/images'));
+    set(ref, {
+      longitude: longitude,
+      latitude: latitude,
+      imageLink: imageLink,
+      time: new Date().getTime(),
+    });
+    console.log('Data Pushed');
+  } catch (error) {
+    console.error('Error pushing data to Firebase:', error);
+  }
 }
